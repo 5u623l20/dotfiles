fdead50b318f3ce33b723916fca8fee624c2f672
Muhammad Moinur Rahman <moin@bofh.im>
2024-01-27 01:58:02 +0100

    Replace e_success with e_info_end function

--- home/private_dot_config/bash/libs/utils.sh
+++ home/private_dot_config/bash/libs/utils.sh
@@ -14,7 +14,6 @@
 
 ########################## Styled text output ##################################
 
-# e_success "I am a success message"
 # e_error "I am an error message"
 # e_warning "I am a warning message"
 # e_underline "I am underlined text"
@@ -25,7 +24,7 @@
 
 # seek_confirmation "Do you want to print a success message?"
 # if is_confirmed; then
-#   e_success "Here is a success message"
+#   e_info_end "Here is a success message"
 # else
 #   e_error "You did not ask for a success message"
 # fi
@@ -33,14 +32,14 @@
 ############ Testing if packages, apps, gems, etc. are installed ###############
 
 # if type_exists 'git'; then
-#   e_success "Git good to go"
+#   e_info_end "Git good to go"
 # else
 #   e_error "Git should be installed. It isn't. Aborting."
 #   exit 1
 # fi
 
 # if is_os "darwin"; then
-#   e_success "You are on a mac"
+#   e_info_end "You are on a mac"
 # else
 #   e_error "You are not on a mac"
 #   exit 1
@@ -103,8 +102,6 @@
 #
 # Headers and Logging
 #
-#e_success() { printf "${green}✔ %s${reset}\n" "$@"
-#}
 #e_error() { printf "${red}✖ %s${reset}\n" "$@"
 #}
 #e_warning() { printf "${tan}➜ %s${reset}\n" "$@"
